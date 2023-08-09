a405d0e8250160e78db57d3f61cd0c37199d88e6
Veronika2007931 <113263850+Veronika2007931@users.noreply.github.com>
2023-08-09 16:50:11 +0300

    додає дз

--- src/index.js
+++ src/index.js
@@ -17,20 +17,49 @@
 
 // #4
 
-const input = document.querySelector('.inputEl')
-const button = document.querySelector('.button')
+// const input = document.querySelector('.inputEl')
+// const button = document.querySelector('.button')
 
-const value = input.value
-button.addEventListener("click", vievAlert)
+// const value = input.value
+// button.addEventListener("click", vievAlert)
 
-function vievAlert(){
-  const now = setInterval(() => {
-    alert('hello')
-    }, value);
-}
+// function vievAlert(){
+//   const now = setInterval(() => {
+//     alert('hello')
+//     }, value);
+// }
 
 
 // Не бачить input.value
+
+// №2
+
+const circle = document.querySelector('.box')
+
+function getNumber(numbers){
+
+  setInterval(()=>{
+    const number =  Math.floor(Math.random() * numbers);
+    circle.style.backgroundColor = "green"
+    circle.style.height = `${number} px`
+    circle.style.width =  `${number} px`
+    console.log(number)
+    console.log( circle.style.height)
+    console.log( circle.style.width)
+}, 4000)
+}
+
+// не змінює розмір
+
+// getNumber(40)
+console.log('hello')
+
+
+
+
+
+
+
 
 
 
