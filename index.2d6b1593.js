5cf096ef71002f26c079cc275719d712bd19874c
Veronika2007931 <113263850+Veronika2007931@users.noreply.github.com>
2023-08-31 14:41:55 +0000

    Deploying to gh-pages from @ Veronika2007931/hw-9@117bf70ff6455742be59b0f7e81feda9cf759cb8 🚀

---
+++ index.2d6b1593.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".inputEl");document.querySelector(".button").addEventListener("click",(function(){const t=e.value;console.log(t),setTimeout((()=>{alert("hello")}),t)}));
+//# sourceMappingURL=index.2d6b1593.js.map
---
+++ index.2f4eade9.js
@@ -0,0 +1,2 @@
+!function(){const e=document.querySelector(".inputEl");document.querySelector(".button").addEventListener("click",(function(){const t=e.value;console.log(t),setTimeout((()=>{alert("hello")}),t)}))}();
+//# sourceMappingURL=index.2f4eade9.js.map
